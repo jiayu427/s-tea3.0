acf829b90cceb36e07461536968175b020d5fe84
celeskyking <celeskyking@163.com>
2013-05-28 23:55:31 +0800

    add

---
+++ LittleMM/src/main/java/com/github/lmm/browser/IBrowser.java
@@ -0,0 +1,11 @@
+package com.github.lmm.browser;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: celeskyking
+ * Date: 13-5-28
+ * Time: 下午9:38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IBrowser {
+}
